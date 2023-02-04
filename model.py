8162fe191296969402a04bdcf405b0793a948fe8
Dev Gurung <dev.gurung141@gmail.com>
2023-02-04 15:33:53 -0600

    added function for modeling

--- model.py
+++ model.py
@@ -32,76 +32,219 @@ seed = 42
 
 a = .05
 
+def get_baseline_accuracy(x_trains, y_train):
+    '''get baseline accuracy score'''
+    
+    # assign most common class to baseline
+    baseline = y_train.mode()
+    
+    # compare baseline with y_train class to get most common class
+    matches_baseline_prediction = (y_train == 6)
+    
+    # get mean
+    baseline_accuracy = matches_baseline_prediction.mean()
+    
+    # print baseline accuracy
+    print(f"Baseline accuracy: {baseline_accuracy}")
 
-def get_tree(x_train, x_validate, y_train, y_validate):
+
+    
+def decision_tree_loop(x_trains, x_validates, y_train, y_validate):
     
     ''' This method pulls in a x_train, x_validate, y_train, y_validate dataframes and returns a dataframe which holds the train accuracy score, validate score and the accuracy difference ''' 
     
+    # create an empty list to append output
+    metrics = []
+
+    for i in range(1,10):
+    # create model
+        clf = DecisionTreeClassifier(max_depth=i, random_state=42)
+
+        # fit the model to training data
+        clf.fit(x_trains, y_train)
+
+        # accuracy score on train
+        accuracy_train = clf.score(x_trains,y_train)
+
+        # accuracy score on validate
+        accuracy_validate = clf.score(x_validates,y_validate)
+
+        output = {'max_depth': i,
+                 'train_accuracy': accuracy_train,
+                 'validate_accuracy': accuracy_validate,
+                 }
+        metrics.append(output)
     
-    results = []
-    for md in range(1, 11):
-        clf = DecisionTreeClassifier(max_depth=md, random_state=42)
-        clf.fit(x_train, y_train)
+    df = pd.DataFrame(metrics)
+    df['difference'] = df.train_accuracy - df.validate_accuracy
+    return df
+
+def random_forest_tree_loop(x_trains, x_validates, y_train, y_validate):
     
-        train_acc = clf.score(x_train, y_train)
-        validate_acc = clf.score(x_validate, y_validate)
-        accuracy_diff = train_acc - validate_acc
-        
-        results.append([md, train_acc, validate_acc, accuracy_diff])
-        
-    results_df = pd.DataFrame(results, columns=['Max Depth', 'Train Accuracy', 'Validate Accuracy', 'Accuracy Difference'])
-    return results_df
-
-def get_forest(x_train, x_validate, y_train, y_validate):
-    results = []
-    for md in range(1, 11):
-            rf = RandomForestClassifier(bootstrap=True, 
-                            class_weight=None, 
-                            criterion='gini',
-                            min_samples_leaf=1,
-                            n_estimators=100,
-                            max_depth=md, 
-                            random_state=42)
-
-        
-            rf.fit(x_train, y_train)
+    # create an empty list to append output
+    metrics = []
     
-            train_acc = rf.score(x_train, y_train)
-            validate_acc = rf.score(x_validate, y_validate)
-            accuracy_diff = train_acc - validate_acc
-        
-            results.append([md, train_acc, validate_acc, accuracy_diff])
-        
-    results_df = pd.DataFrame(results, columns=['Max Depth', 'Train Accuracy', 'Validate Accuracy', 'Accuracy Difference'])
-    return results_df
-
-
-def get_knn(x_train, x_validate, y_train, y_validate):
-    results = []
-    for n in range(1, 100):
-            knn = KNeighborsClassifier(n_neighbors=n, weights='uniform')
-        
-            knn.fit(x_train, y_train)
+    for i in range(1, 25):
     
-            train_acc = knn.score(x_train, y_train)
-            validate_acc = knn.score(x_validate, y_validate)
-            accuracy_diff = train_acc - validate_acc
-        
-            results.append([n, train_acc, validate_acc, accuracy_diff])
-        
-    results_df = pd.DataFrame(results, columns=['Max Depth', 'Train Accuracy', 'Validate Accuracy', 'Accuracy Difference'])
-    return results_df
+        # create model
+        rf = RandomForestClassifier(min_samples_leaf =i, random_state=42) 
+
+        # fit the model to training data
+        rf.fit(x_trains, y_train)
+
+        # accuracy score on train
+        accuracy_train = rf.score(x_trains,y_train)
+
+        # accuracy score on validate
+        accuracy_validate = rf.score(x_validates,y_validate)
+
+        output = {'min_samples_leaf ': i,
+                 'train_accuracy': accuracy_train,
+                 'validate_accuracy': accuracy_validate,
+                 }
+        metrics.append(output)
+    
+    df = pd.DataFrame(metrics)
+    df['difference'] = df.train_accuracy - df.validate_accuracy
+    return df
 
 
+def knn_loop(x_trains, x_validates, y_train, y_validate):
+    
+    # create an empty list to append output
+    metrics = []
+    
+    for i in range(1, 25):
 
-def get_tree_test(x_train, x_test, y_train, y_test,md):
+        # create model
+        knn = KNeighborsClassifier(n_neighbors=i) 
+
+        # fit the model to training data
+        knn.fit(x_trains, y_train)
+
+        # accuracy score on train
+        accuracy_train = knn.score(x_trains,y_train)
+
+        # accuracy score on validate
+        accuracy_validate = knn.score(x_validates,y_validate)
+
+        output = {'n_neighbors': i,
+                 'train_accuracy': accuracy_train,
+                 'validate_accuracy': accuracy_validate,
+                 }
+        metrics.append(output)
+    
+    df = pd.DataFrame(metrics)
+    df['difference'] = df.train_accuracy - df.validate_accuracy
+    return df
+
+
+def get_decision_tree(x_trains, x_validates, y_train, y_validate, n):
+    '''get decision tree accuracy score on train and validate data'''
+    
+    # create model
+    clf = DecisionTreeClassifier(max_depth = n, random_state=42)
+
+    # fit the model to train data
+    clf.fit(x_trains, y_train)
+
+    # compute accuracy
+    train_acc = clf.score(x_trains, y_train)
+    validate_acc = clf.score(x_validates, y_validate)
+    
+    # print accuracy score on train
+#     print(f'Decision Tree Accuracy score on train set: {train_acc}')
+    
+    # print accuracy score on validate
+#     print(f'Decsion Tee Accuracy score on validate set: {validate_acc}')
+    
+    return train_acc, validate_acc
+
+
+def get_random_forest(x_trains, x_validates, y_train, y_validate, n):
+    '''get random forest accuracy score on train and validate data'''
+    
+    # create model
+    rf= RandomForestClassifier(min_samples_leaf = n, random_state=42) 
+
+    # fit the model to train data
+    rf.fit(x_trains, y_train)
+
+    # compute accuracy
+    train_acc = rf.score(x_trains, y_train)
+    validate_acc = rf.score(x_validates, y_validate)
+    
+    # print accuracy score on train
+#     print(f'Random Forest Accuracy score on train set: {train_acc}')
+    
+    # print accuracy score on validate
+#     print(f'Random Forest score on validate set: {validate_acc}')
+    return train_acc, validate_acc
+
+
+def get_knn(x_trains, x_validates, y_train, y_validate, n):
+    ''' get KNN accuracy score on train and validate data'''
+    
+    # create model
+    knn= KNeighborsClassifier(n_neighbors = n) 
+
+    # fit the model to train data
+    knn.fit(x_trains, y_train)
+
+    # compute accuracy
+    train_acc = knn.score(x_trains, y_train)
+    validate_acc = knn.score(x_validates, y_validate)
+    
+    # print accuracy score on train
+#     print(f'KNN Accuracy score on train set: {train_acc}')
+    
+    # print accuracy score on validate
+#     print(f'KNN Accuracy score on validate set: {validate_acc}')
+    
+    return train_acc, validate_acc
+    
+def get_models_accuracy(x_trains, y_train, x_validates, y_validate, train, validate):
+    '''takesx_trains, y_train, x_validates, y_validate, train, validate, target
+    return dataframe with models and their RMSE values on train and validate data
+    '''
+    # get accuracy
+#     baseline_accuracy = mo.get_baseline_accuracy(x_trains, y_train)
+    tree_train_acc, tree_validate_acc= get_decision_tree(x_trains, x_validates, y_train, y_validate, 2)
+    random_train_acc, random_validate_acc= get_random_forest(x_trains, x_validates, y_train, y_validate, 22)
+    knn_train_acc, knn_validate_acc= get_knn(x_trains, x_validates, y_train, y_validate, 19)
+    
+    # assing index
+    index = ['Decision_Tree(max_depth=2)', 'Random_Forest(min_samples_lead=22)', 'KNN (Neighours=19)']
+    
+    # create a dataframe
+    df = pd.DataFrame({'train_accuracy':[tree_train_acc, random_train_acc, knn_train_acc],
+                         'validate_accuracy': [tree_validate_acc, random_validate_acc, knn_validate_acc]},index=index)
+    df['difference']= df['train_accuracy']-df['validate_accuracy']
+    
+    return df
+
+
+def get_decison_tree_test(x_train, x_test, y_train, y_test,n):
    
 
-        clf = DecisionTreeClassifier(max_depth=md, random_state=42)
+        clf = DecisionTreeClassifier(max_depth=n, random_state=42)
         clf.fit(x_train, y_train)
     
        
         validate_acc = clf.score(x_test, y_test)
+        
+        
+        print(validate_acc)
+        
+        
+def get_random_forest_test(x_train, x_test, y_train, y_test,n):
+   
+
+        rf= RandomForestClassifier(min_samples_leaf = n, random_state=42) 
+        rf.fit(x_train, y_train)
+    
+       
+        validate_acc = rf.score(x_test, y_test)
         
         
         print(validate_acc)
