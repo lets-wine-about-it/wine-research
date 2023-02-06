ca3a5263dca7e6676d9894c2df88408bbac2fe84
Dev Gurung <dev.gurung141@gmail.com>
2023-02-05 18:04:46 -0600

    updated with markdown

--- prepare.py
+++ prepare.py
@@ -9,30 +9,56 @@ from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer
 
-
 seed =42
 
 
-def get_bins(df):
-      
-    new_data = { 3:"poor",
-             4:"poor",
-             5:"average",
-             6:"average",
-             7:"good",
-             8:"good",
-             9:"good"}
+def wine_data():
+    '''read two csv files separately from local storage and return a dataframe made of combined data of csv files'''
     
-    # combine this new data with existing DataFrame
-    df["quality_bin"] = df["quality"].map(new_data)
-    
-    return df
-    
-    
+    # read data from csv to a dataframe
+    red = pd.read_csv('winequality-red.csv')
+    white = pd.read_csv('winequality-white.csv')
 
+    # create a new column for a dataframe
+    red["type"] = 'red'
+    white['type'] = 'white'
+
+    # concate dataframe 
+    wines = pd.concat([red, white], axis=0)
+    
+    # create a dictionary
+    name_dict = {'fixed acidity':'fixed_acidity', 'volatile acidity': 'volatile_acidity','citric acid':'citric_acid','residual sugar':'residual_sugar','free sulfur dioxide':'free_sulfur_dioxide','total sulfur dioxide':'total_sulfur_dioxide'}
+    
+    # rename columns
+    wines.rename(columns = name_dict, inplace = True)
+    
+    return wines
+
+
+# def get_bins(df):
+#     take
+      
+#     new_data = { 3:"poor",
+#              4:"poor",
+#              5:"average",
+#              6:"average",
+#              7:"good",
+#              8:"good",
+#              9:"good"}
+    
+#     # combine this new data with existing DataFrame
+#     df["quality_bin"] = df["quality"].map(new_data)
+    
+#     return df
+    
+    
 def get_dummies(df, columns):
+    '''takes in a dataframe and columns names and return  dataframe with encoded vairables'''
+    
     df_dummies = pd.get_dummies(df, columns=columns, prefix=columns, prefix_sep='_', drop_first=True)
+    
     return df_dummies
+
 
 def split_data(df,strat):
     '''
@@ -40,30 +66,40 @@ def split_data(df,strat):
     return train, validate, test DataFrames.
     '''
     
+    df = get_dummies(df,['type'])
+    
     train_validate, test = train_test_split(df, test_size=.2, random_state=42,stratify= df[strat])
     
-    train, validate = train_test_split(train_validate, 
-                                       test_size=.3, 
-                                       random_state=42,stratify= train_validate[strat] )
+    train, validate = train_test_split(train_validate, test_size=.3, random_state=42,stratify= train_validate[strat] )
     
-#     print(train.shape , validate.shape, test.shape)
-
-          
     return train, validate, test
 
 
 def train_val_test(df, strat):
-    df = get_bins(df)
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames.
+    return train, validate, test DataFrames.
+    '''
     
-    df= get_dummies(df)
+    # create a dictionary
+    new_data = { 3:"poor",
+             4:"average",
+             5:"average",
+             6:"average",
+             7:"average",
+             8:"good",
+             9:"good"}
+    
+    # combine this new data with existing DataFrame
+    df["quality_bin"] = df["quality"].map(new_data)
+    
+#     df= get_dummies(df)
     
     train, validate, test = split_data(df,strat)
     
     return train, validate, test
     
     
-
-
 def x_and_y(train,validate,test,target):
     
     """
@@ -82,11 +118,9 @@ def x_and_y(train,validate,test,target):
     return x_train, y_train, x_validate, y_validate, x_test, y_test
 
 
-def scale_data(train, validate, test, 
-               columns_to_scale=[],
-               return_scaler=False):
+def scale_data(train, validate, test, columns_to_scale=[], return_scaler=False):
     ''' 
-    Takes in train, validate, and test data and returns their scaled counterparts.
+    takes in train, validate, and test data and returns their scaled counterparts.
     If return_scalar is True, the scaler object will be returned as well
     '''
     # make copies of our original data so we dont gronk up anything
@@ -105,7 +139,7 @@ def scale_data(train, validate, test,
                                                   columns=train[columns_to_scale].columns.values).set_index([train.index.values])
                                                   
     validate_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(validate[columns_to_scale]),
-                                                  columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
+                                             columns=validate[columns_to_scale].columns.values).set_index([validate.index.values])
     
     test_scaled[columns_to_scale] = pd.DataFrame(scaler.transform(test[columns_to_scale]),
                                                  columns=test[columns_to_scale].columns.values).set_index([test.index.values])
@@ -115,5 +149,3 @@ def scale_data(train, validate, test,
     else:
         return train_scaled, validate_scaled, test_scaled
     
-
-
